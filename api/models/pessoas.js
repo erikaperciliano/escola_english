ef0e1c617c91430389380be801404e795232faed
Erika Perciliano <48223561+erikaperciliano@users.noreply.github.com>
2021-04-29 17:36:24 -0300

    filtrando todas as matrículas com status confirmado
    Utilizado um escopo de associação

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -39,9 +39,12 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: 'docente_id'
     }) 
     Pessoas.hasMany(models.Matriculas, {
-      foreignKey: 'estudante_id'
+      foreignKey: 'estudante_id',
+      scope: {
+        status: 'confirmado'
+      },
+      as: 'aulasMatriculadas'
     })
-
   }
   return Pessoas
 }
