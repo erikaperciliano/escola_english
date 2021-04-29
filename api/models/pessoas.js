35ccaf59274fd9cb9875e43ef56a40451915e3e5
Erika Perciliano <48223561+erikaperciliano@users.noreply.github.com>
2021-04-29 17:03:32 -0300

    Validação de  nome

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,7 +1,14 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Pessoas = sequelize.define('Pessoas', {
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        fucaoValidadora: function(dado){
+          if(dado.length < 3) throw new Error('O campo nome deve ter mais de 3 caracteres!')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     email: {
       type:DataTypes.STRING,
