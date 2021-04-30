c506c81a19634368f3f103dbfe87addbf1223faf
Erika Perciliano <48223561+erikaperciliano@users.noreply.github.com>
2021-04-30 17:21:08 -0300

    separando os arquivos

---
+++ api/services/TurmasServices.js
@@ -0,0 +1,10 @@
+const Services = require('./Services');
+
+class TurmasServices extends Services {
+    constructor(){
+        super('Pessoas');
+    }
+    //métodos específicos do controlador de Pessoas
+}
+
+module.exports = TurmasServices;
